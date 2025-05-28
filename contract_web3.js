45b3c385c806cf93b2d5c60567ce250cb7deeb9f
GILANG <153954117+ggg3ya@users.noreply.github.com>
2025-05-29 01:41:05 +0700

    Create contract_web3.js

---
+++ contract_web3.js
@@ -0,0 +1,59 @@
+export const PHAROS_RPC = "https://testnet.dplabs-internal.com";
+export const CHAIN_ID = 688688;
+
+export const WPHRS_ADDRESS = "0x76aaada469d23216be5f7c596fa25f282ff9b364";
+export const USDC_ADDRESS = "0xad902cf99c2de2f1ba5ec4d642fd7e49cae9ee37";
+export const SWAP_ROUTER_ADDRESS = "0x1a4de519154ae51200b0ad7c90f7fac75547888a";
+
+// Uniswap V3 style router ABI
+export const SWAP_ROUTER_ABI = [
+    'function multicall(uint256, bytes[]) public payable returns (bytes[] memory)',
+    'function exactInputSingle((address,address,uint24,address,uint256,uint256,uint160)) external payable returns (uint256)',
+];
+
+
+
+// Uniswap V2 style router ABI (for swapExactETHForTokens, getAmountsOut, etc.)
+export const ROUTER_ABI = [
+  {
+    inputs: [
+      { internalType: "uint256", name: "amountIn", type: "uint256" },
+      { internalType: "address[]", name: "path", type: "address[]" },
+    ],
+    name: "getAmountsOut",
+    outputs: [
+      { internalType: "uint256[]", name: "amounts", type: "uint256[]" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "amountOutMin", type: "uint256" },
+      { internalType: "address[]", name: "path", type: "address[]" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+    ],
+    name: "swapExactETHForTokens",
+    outputs: [
+      { internalType: "uint256[]", name: "amounts", type: "uint256[]" },
+    ],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "amountIn", type: "uint256" },
+      { internalType: "uint256", name: "amountOutMin", type: "uint256" },
+      { internalType: "address[]", name: "path", type: "address[]" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+    ],
+    name: "swapExactTokensForTokens",
+    outputs: [
+      { internalType: "uint256[]", name: "amounts", type: "uint256[]" },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
